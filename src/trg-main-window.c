bda3a28d5b6be86b93e8e22fd024f085342905cb
Alan Fitton <ajf@eth0.org.uk>
2011-02-07 21:24:34 +0000

    more icon fixes

--- src/trg-main-window.c
+++ src/trg-main-window.c
@@ -1275,6 +1275,8 @@ static GObject *trg_main_window_constructor(GType type,
     GtkWidget *w;
     GtkWidget *outerVbox;
     GtkWidget *toolbarHbox;
+    GtkIconTheme *theme;
+    GdkPixbuf *icon;
     gint width, height;
 
     self = TRG_MAIN_WINDOW(G_OBJECT_CLASS
@@ -1283,7 +1285,12 @@ static GObject *trg_main_window_constructor(GType type,
 			    construct_params));
     priv = TRG_MAIN_WINDOW_GET_PRIVATE(self);
 
-    gtk_window_set_icon_name(GTK_WINDOW(self), PACKAGE_NAME);
+    theme = gtk_icon_theme_get_default();
+    icon = gtk_icon_theme_load_icon (theme, PACKAGE_NAME, 48, GTK_ICON_LOOKUP_USE_BUILTIN, NULL);
+
+    if (icon)
+    	gtk_window_set_default_icon(icon);
+
     gtk_window_set_title(GTK_WINDOW(self), PACKAGE_NAME);
     gtk_container_set_border_width(GTK_CONTAINER(self), 5);
     gtk_window_set_default_size(GTK_WINDOW(self), 1000, 600);
@@ -1385,7 +1392,7 @@ static GObject *trg_main_window_constructor(GType type,
     priv->notebook = trg_main_window_notebook_new(self);
     gtk_paned_pack2(GTK_PANED(priv->vpaned), priv->notebook, FALSE, FALSE);
 
-    priv->statusIcon = gtk_status_icon_new_from_file(PACKAGE_NAME);
+    priv->statusIcon = gtk_status_icon_new_from_pixbuf(icon);
     gtk_status_icon_set_screen(priv->statusIcon,
 			       gtk_window_get_screen(GTK_WINDOW(self)));
     g_signal_connect(priv->statusIcon, "activate",
