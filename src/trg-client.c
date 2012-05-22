2021397c9fc5de8c0ba0ae665f0d771e6d80a9d7
Alan Fitton <alan@eth0.org.uk>
2012-05-22 08:53:42 +0100

    Swedish translation. Thanks Åke Svensson

--- src/trg-client.c
+++ src/trg-client.c
@@ -1,5 +1,5 @@
 /*
- * transmission-remote-gtk - A GTK RPC tc to Transmission
+ * transmission-remote-gtk - A GTK RPC client to Transmission
  * Copyright (C) 2011  Alan Fitton
 
  * This program is free software; you can redistribute it and/or modify
