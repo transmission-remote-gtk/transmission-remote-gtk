db0223dd3baf384825e6671612dd96c9d6677b38
LaserEyess <LaserEyess@users.noreply.github.com>
2022-01-21 19:43:24 -0500

    remove win32 mailslots
    These do not seem necessary for gtk3 or gtk4, the same APIs should work
    on windows as they do on linux.

--- src/main.c
+++ src/main.c
@@ -19,40 +19,22 @@
 
 #include "config.h"
 
-#include <stdlib.h>
-
 #include <curl/curl.h>
 #include <curl/easy.h>
 
 #include <glib/gi18n.h>
 #include <glib-object.h>
 #include <gtk/gtk.h>
-#include <json-glib/json-glib.h>
 #include <fontconfig/fontconfig.h>
 
 #include "trg-gtk-app.h"
-#ifdef G_OS_WIN32
-#include "win32-mailslot.h"
-#endif
 
 #include "trg-main-window.h"
 #include "trg-client.h"
-#include "util.h"
 
-/* Handle arguments and start the main window.
- *
- * either GtkApplication - replaces libunique, GTK3 only, and non-win32.
- * or win32 API mailslots.
- *
- * win32 could possibly run from GtkApplication now, mailslots were needed
- * for GTK2 (support removed).
- */
+/* Handle arguments and start the main window. */
 
 #ifndef G_OS_WIN32
-
-/* GtkApplication - the replacement for libunique.
- * This is implemented in trg-gtk-app.c
- */
 
 static gint trg_gtkapp_init(TrgClient * client, int argc, char *argv[])
 {
@@ -68,7 +50,7 @@ static gint trg_gtkapp_init(TrgClient * client, int argc, char *argv[])
 #elif defined G_OS_WIN32
 
 static gint
-trg_win32_init(TrgClient * client, int argc, char *argv[], gchar ** args)
+trg_win32_init(TrgClient * client, int argc, char *argv[])
 {
     gchar *moddir =
         g_win32_get_package_installation_directory_of_module(NULL);
@@ -80,16 +62,9 @@ trg_win32_init(TrgClient * client, int argc, char *argv[], gchar ** args)
     g_free(moddir);
     g_free(localedir);
 
-    if (!mailslot_send_message(args)) {
-        TrgMainWindow *window =
-            trg_main_window_new(client, should_be_minimised(argc, argv));
-        trg_main_window_set_start_args(window, args);
-        auto_connect_if_required(window);
-        mailslot_start_background_listener(window);
-        gtk_main();
-    }
-
-    return EXIT_SUCCESS;
+    TrgGtkApp *gtk_app = trg_gtk_app_new(client);
+    gint exitCode = g_application_run(G_APPLICATION(gtk_app), argc, argv);
+    return exitCode;
 }
 
 #else
@@ -108,8 +83,6 @@ trg_simple_init(TrgClient * client, int argc, char *argv[], gchar ** args)
 
 #endif
 
-/* Win32 mailslots. I've implemented this in win32-mailslot.c */
-
 #ifndef G_OS_WIN32
 static void trg_non_win32_init(void)
 {
@@ -122,62 +95,12 @@ static void trg_cleanup(void)
     curl_global_cleanup();
 }
 
-#ifdef G_OS_WIN32
-
-static gchar **convert_args(int argc, char *argv[])
-{
-    gchar *cwd = g_get_current_dir();
-    gchar **files = NULL;
-
-    if (argc > 1) {
-        GSList *list = NULL;
-        int i;
-
-        for (i = 1; i < argc; i++) {
-            if (is_minimised_arg(argv[i])) {
-                continue;
-            } else if (!is_url(argv[i]) && !is_magnet(argv[i])
-                       && g_file_test(argv[i], G_FILE_TEST_IS_REGULAR)
-                       && !g_path_is_absolute(argv[i])) {
-                list = g_slist_append(list,
-                                      g_build_path(G_DIR_SEPARATOR_S, cwd,
-                                                   argv[i], NULL));
-            } else {
-                list = g_slist_append(list, g_strdup(argv[i]));
-            }
-        }
-
-        if (list) {
-            GSList *li;
-            files = g_new0(gchar *, g_slist_length(list) + 1);
-            i = 0;
-            for (li = list; li; li = g_slist_next(li)) {
-                files[i++] = li->data;
-            }
-            g_slist_free(list);
-        }
-    }
-
-    g_free(cwd);
-
-    return files;
-}
-
-#endif
-
 int main(int argc, char *argv[])
 {
-#ifdef G_OS_WIN32
-    gchar **args;
-#endif
     gint exitCode = EXIT_SUCCESS;
     TrgClient *client;
 
     gtk_init(&argc, &argv);
-
-#ifdef G_OS_WIN32
-    args = convert_args(argc, argv);
-#endif
 
     curl_global_init(CURL_GLOBAL_ALL);
     client = trg_client_new();
@@ -187,7 +110,7 @@ int main(int argc, char *argv[])
     textdomain(GETTEXT_PACKAGE);
 
 #ifdef G_OS_WIN32
-    exitCode = trg_win32_init(client, argc, argv, args);
+    exitCode = trg_win32_init(client, argc, argv);
 #else
     trg_non_win32_init();
     exitCode = trg_gtkapp_init(client, argc, argv);
