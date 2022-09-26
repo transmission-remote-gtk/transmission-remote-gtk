a09e4d4c09e34b4c60df2506dfc76ef4d1b2aa40
LaserEyess <16581533+LaserEyess@users.noreply.github.com>
2022-09-26 16:52:13 -0400

    Remove RSS support
    RSS support is blocking a few simplifications for some other
    improvements, and it additionally is very much untested and relies on
    old, abandoned libraries for XML parsing.
    
    Other technical factors influenced this decision (see github issue #191)
    but the primary motivation is developer time and knowledge to maintain
    this support. Frankly, there is none. Patches welcome.

--- extern/rss-glib/rss-glib.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RSS_GLIB_H__
-#define __RSS_GLIB_H__
-
-#include <rss-glib/rss-parser.h>
-#include <rss-glib/rss-document.h>
-#include <rss-glib/rss-item.h>
-#include <rss-glib/rss-version.h>
-
-#endif /* __RSS_GLIB_H__ */
